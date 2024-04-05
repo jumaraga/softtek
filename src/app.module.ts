b8629483939cdbda16fa055e2ec846d40bda0c60
Josue Ramirez <josue.ramirez@uni.pe>
2024-04-05 08:31:18 -0500

    - feat: add character dynamo table

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { HttpModule } from '@nestjs/axios';
 import { ConfigModule } from '@nestjs/config';
 import config from './config/config';
 import { FilmModule } from './films/films.module';
